49d83ae072a7d1e39dfad8a14efcf6f93ac2b8ef
SleepyJen <jennifer.ni0924@gmail.com>
2019-11-07 19:48:19 -0800

    added the form to add a train and connected to firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDTlyieE7mHSaS-DIFHWEOuETJq35Tyu-s",
+    authDomain: "trainscheduler-f0ba4.firebaseapp.com",
+    databaseURL: "https://trainscheduler-f0ba4.firebaseio.com",
+    projectId: "trainscheduler-f0ba4",
+    storageBucket: "trainscheduler-f0ba4.appspot.com",
+    messagingSenderId: "721700363398",
+    appId: "1:721700363398:web:c915a7bbd6da5dcb224507"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
